2323fa601a41d02fcdf1b3b940431572310bedde
Shresth Raj <shresthraj1012001@gmail.com>
2025-06-08 01:55:33 +0530

    Update About.js

--- src/Components/Scripts/About.js
+++ src/Components/Scripts/About.js
@@ -19,9 +19,8 @@ export const About = () => {
 	}, []);
 	const [text] = useTypewriter({
 		words: [
-			"Full Stack Developer",
-			"ReactJs Developer",
-			"Java Developer",
+			"a passionate Full Stack Developer dedicated to building robust, scalable, and user-centric web applications.
+"
 		],
 		loop: {},
 	});
@@ -34,18 +33,16 @@ export const About = () => {
 					Shresth Raj
 				</p>
 				<div className={styles.title}>
-					I am a{" "}
+					I am {" "}
 					<span className={styles.typewriter}>
 						{text}
 						<Cursor />
 					</span>
 				</div>
 				<div className={styles.bio}>
-					I am a motivated and versatile individual, always eager to
-					take on new challenges. With a passion for learning I am
-					dedicated to delivering high-quality results. With a
-					positive attitude and a growth mindset, I am ready to make a
-					meaningful contribution and achieve great things.
+					I enjoy working across the stack, from designing intuitive user interfaces to architecting efficient backend systems. Proficient in technologies like React.js, JavaScript, Node.js, and Firebase, I thrive on solving complex problems, optimizing performance, and collaborating with teams to turn ideas into reliable, production-ready solutions. With a growth mindset and a strong attention to detail, I aim to deliver applications that are not only functional and performant but also maintainable and accessible.
+
+Let’s create something impactful together.
 				</div>
 				<a
 					href="https://drive.google.com/file/d/1pHIilS6qy5GX0NcyqF0aZYwSYXJTSk_4/view?usp=drive_link"
